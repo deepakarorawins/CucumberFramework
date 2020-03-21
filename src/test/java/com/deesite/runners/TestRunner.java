528e90b34dea75d255dfd8e558561d8d0b8981f6
Deepak Arora <deepakarora@Deepaks-MacBook-Pro.local>
2020-03-21 09:39:34 +0530

    Cucumber Reports

--- src/test/java/com/deesite/runners/TestRunner.java
+++ src/test/java/com/deesite/runners/TestRunner.java
@@ -7,9 +7,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources/functionalTests",
-		glue= {"stepDefinitions"}
-		)
+		features = "src/test/resources/functionalTests", 
+		glue = { "stepDefinitions" },
+		//plugin = {"usage" },
+		plugin = {"pretty", 
+				"json:target/cucumber-reports/Cucumber.json",
+				"junit:target/cucumber-reports/Cucumber.xml",
+				"html:target/cucumber-reports"}, 
+		monochrome = true)
 public class TestRunner {
 
 }
